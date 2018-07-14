f1dc6a4bb6856497931f7948a9699e423af88a93
Nick Jordan <njordan1017@gmail.com>
2018-07-14 12:23:06 -0500

    have working prototype of angular login

--- NewFantasyUI/src/main/java/com/fantasy/ui/NewFantasyUiApplication.java
+++ NewFantasyUI/src/main/java/com/fantasy/ui/NewFantasyUiApplication.java
@@ -1,12 +1,12 @@
-package com.fantasy.dataaccessutility;
+package com.fantasy.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataAccessUtilityApplication {
+public class NewFantasyUiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DataAccessUtilityApplication.class, args);
+		SpringApplication.run(NewFantasyUiApplication.class, args);
 	}
 }
