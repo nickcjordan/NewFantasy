6d892316213d3e0bebc0b888fbfb763b577da590
Nick Jordan <njordan1017@gmail.com>
2018-11-15 08:52:31 -0600

    still trying

--- NewFantasyUI/src/environments/environment.prod.ts
+++ NewFantasyUI/src/environments/environment.prod.ts
@@ -1,5 +1,10 @@
+//export const environment = {
+//  production: true,
+//  uiServer: 'http://www.falifa-ui.us-east-2.elasticbeanstalk.com/ui',
+//  apiServer: 'http://www.falifa-db.us-east-2.elasticbeanstalk.com/api'
+//};
 export const environment = {
   production: true,
-  uiServer: 'http://www.falifa-ui.us-east-2.elasticbeanstalk.com/ui',
-  apiServer: 'http://www.falifa-db.us-east-2.elasticbeanstalk.com/api'
+  uiServer: 'https://8c0klvlndl.execute-api.us-east-2.amazonaws.com/beta/ui',
+  apiServer: 'https://8c0klvlndl.execute-api.us-east-2.amazonaws.com/beta/api'
 };
