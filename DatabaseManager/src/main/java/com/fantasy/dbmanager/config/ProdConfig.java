7b7a385e9baf6bf536391eb4fc0845f122b71792
Nick Jordan <njordan1017@gmail.com>
2018-11-27 14:59:27 -0600

    ugh

--- DatabaseManager/src/main/java/com/fantasy/dbmanager/config/ProdConfig.java
+++ DatabaseManager/src/main/java/com/fantasy/dbmanager/config/ProdConfig.java
@@ -3,6 +3,7 @@ package com.fantasy.dbmanager.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -37,6 +38,11 @@ public class ProdConfig {
 	@Bean(name = "testString")
 	public String testString() {
 		return "prod";
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 	
     @SuppressWarnings("deprecation")
